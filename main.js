36b2f92d8debe0f3d0b49bf4087c1668025ec7aa
Asbeyy <114877785+Asbeyy@users.noreply.github.com>
2023-05-28 18:46:14 +0200

    fix gltf1

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
-import humanBody from '/public/modelli/body_male/scene.gltf'
+import humanBody from '/dist/modelli/body_male/scene.gltf'
 
 
 /**
